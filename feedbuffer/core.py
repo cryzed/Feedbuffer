f44c0bdaaa05821244c2c66c663bf6f37c6f0c4e
cryzed <cryzed@googlemail.com>
2016-03-01 23:40:37 +0100

    Fix some encoding corner cases

--- feedbuffer/core.py
+++ feedbuffer/core.py
@@ -27,11 +27,20 @@ def update_feed(url):
     except requests.exceptions.Timeout:
         return
 
-    soup = bs4.BeautifulSoup(response.text, 'xml')
+    # Don't let requests do the content decoding, instead just hint at the detect encoding and let BeautifulSoup and the
+    # treebuilder do its thing. For example, lxml only correctly parses content with <content:encoded> tags when it can
+    # decode the bytes itself.
+    try:
+        encoding = response.encoding
+        soup = bs4.BeautifulSoup(response.content, 'xml', from_encoding=encoding)
+    except UnicodeDecodeError:
+        encoding = response.apparent_encoding
+        soup = bs4.BeautifulSoup(response.content, 'xml', from_encoding=encoding)
+
     entries = get_feed_entries(soup)
     feed = feedparser.parse(response.text)
     entry_ids = [entry.id for entry in feed.entries]
-    database.update_feed(url, response.text, zip(entry_ids, (str(entry) for entry in entries)))
+    database.update_feed(url, response.content, zip(entry_ids, (str(entry) for entry in entries)), encoding or '')
 
 
 def update_and_reschedule_feed(url):
@@ -69,8 +78,8 @@ def fix_up_feed_soup(soup):
             content.extract()
 
 
-def generate_feed(feed_data, entries):
-    feed = bs4.BeautifulSoup(feed_data, 'xml')
+def generate_feed(feed_data, entries, encoding):
+    feed = bs4.BeautifulSoup(feed_data, 'xml', from_encoding=encoding or None)
     fix_up_feed_soup(feed)
     delete_feed_entries(feed)
     root = feed.find(['rss', 'feed'])
