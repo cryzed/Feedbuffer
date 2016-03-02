72ffbeab9ed49df2020c29b831e8cf2f39b66ff6
cryzed <cryzed@googlemail.com>
2016-03-02 04:59:03 +0100

    Make CherryPy configuration nicer

--- main.py
+++ main.py
@@ -12,9 +12,11 @@ logger = log.get_logger(__name__)
 
 
 def main():
-    cherrypy.config.update({'server.socket_port': PORT, 'server.socket_host': '0.0.0.0'})
-    cherrypy.config['server.socket_port'] = PORT
-    cherrypy.config['checker.check_skipped_app_config'] = False
+    cherrypy.config.update({
+        'server.socket_port': PORT,
+        'server.socket_host': '0.0.0.0',
+        'checker.check_skipped_app_config': False
+    })
     threading.Thread(target=lambda: cherrypy.quickstart(Server())).start()
     while True:
         try:
